b093250808fad5d3f67b801aba9a2d7e72fc7678
Lediankina Anastasia <anastasialediankina@gmail.com>
2023-08-31 08:30:28 +1000

    Echo-Bot

--- LAV_interview_bot.py
+++ LAV_interview_bot.py
@@ -1,3 +1,33 @@
-print('The first message')
+from aiogram import Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import Message
 
-print('The changes')
+API_TOKEN: str = '6537929882:AAHUJ4usGaSk6EiNtVlX3VvsvZUtQTjRtXY'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+
+# Этот хэндлер будет срабатывать на команду "/start"
+@dp.message(Command(commands=["start"]))
+async def process_start_command(message: Message):
+    await message.answer('Привет!\nЭто Эхо-Бот!\nНапиши мне что-нибудь')
+
+
+# Этот хэндлер будет срабатывать на команду "/help"
+@dp.message(Command(commands=['help']))
+async def process_help_command(message: Message):
+    await message.answer('Напиши мне что-нибудь и в ответ '
+                         'я пришлю тебе твое сообщение')
+
+
+# Этот хэндлер будет срабатывать на любые ваши текстовые сообщения,
+# кроме команд "/start" и "/help"
+@dp.message()
+async def send_echo(message: Message):
+    await message.reply(text=message.text)
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
